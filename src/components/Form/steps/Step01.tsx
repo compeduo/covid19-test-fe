9201b8d080a8ca3d1e3339bb0e1410318af745fb
ankerek <ankerek@gmail.com>
2020-03-23 20:25:12 +0100

    add second level spacing

--- src/components/Form/steps/Step01.tsx
+++ src/components/Form/steps/Step01.tsx
@@ -1,10 +1,17 @@
-import { FormGroup, Typography } from '@material-ui/core'
+import { FormGroup, makeStyles, Typography } from '@material-ui/core'
 import { Field } from 'formik'
-import { CheckboxWithLabel, RadioGroup } from 'formik-material-ui'
+import { CheckboxWithLabel } from 'formik-material-ui'
 import React from 'react'
 import { StepProps } from '~/components/Form/steps/index'
 
+const useStyles = makeStyles(theme => ({
+  secondLevel: {
+    marginLeft: theme.spacing(4),
+  },
+}))
+
 export const Step01: React.FC<StepProps> = ({ values }) => {
+  const classes = useStyles()
   return (
     <>
       <Typography variant="h6" gutterBottom>
@@ -24,12 +31,14 @@ export const Step01: React.FC<StepProps> = ({ values }) => {
           Label={{ label: 'Horečka nad 38°C' }}
         />
         {values.fever && (
-          <Field
-            component={CheckboxWithLabel}
-            name="longFever"
-            type="checkbox"
-            Label={{ label: 'Horečka nad 38°C více jak dva dny' }}
-          />
+          <div className={classes.secondLevel}>
+            <Field
+              component={CheckboxWithLabel}
+              name="longFever"
+              type="checkbox"
+              Label={{ label: 'Horečka nad 38°C více jak dva dny' }}
+            />
+          </div>
         )}
         <Field
           component={CheckboxWithLabel}
